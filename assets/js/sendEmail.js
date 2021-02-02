249e3046585e98e39512e164d8972dab099a080b
Mateusz Niechwiej <niechwiej.mateusz@gmail.com>
2021-02-02 10:17:28 +0000

    Implement emailjs integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log('helo');
+function sendMail(contactForm) {
+    emailjs.send("service_fity97k", "rosie", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILLED", error);
+        });
+    return false;// to prevent from refreshing the page
+}
